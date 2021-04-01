6a9c35194f37442d021031a6b49f8495082bacd4
siva4646 <sivabommisetty464@gmail.com>
2021-04-01 17:23:43 +0530

    added new file

---
+++ vscodepython/Basics/third.py
@@ -0,0 +1,65 @@
+print("hello world")
+a = 20
+b =type(a)
+print(b)
+print ("hello \n world")
+mystring = "hello world"
+b = mystring[0]
+print(b)
+b = mystring[::]
+print(b)
+b = mystring[-3]
+print(b)
+b = mystring[1:3]
+print(b)
+b = mystring[:3]
+print(b)
+name = "siva"
+b =  name[1:]
+print(b)
+x = "hello world"
+b = x.upper()
+print(b)
+b = x.lower()
+print(b)
+b = x.split()
+print(b)
+b =  x.split('l')
+print(b)
+siva = "rock"
+if siva == "jerry":
+    print("this is sweet")
+elif siva == "dev":
+        print("this is good")
+else:
+            print("this is sweet")
+mystring = "hello this is siva"
+for num in mystring:
+    if num == "i":
+        print("one of the charector matching")
+    elif num == "l":
+        print("l id satisfy") 
+    else:
+        print("not matching")
+        print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
